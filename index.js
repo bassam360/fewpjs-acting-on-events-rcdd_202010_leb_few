6d4942f3932dafad5475d1668c9fc3979cfcc4ea
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-01 14:52:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
   let leftNumber = dodger.style.left.replace('px', '');
-  let left = parseInt(leftNumbers, 10)
+  let left = parseInt(leftNumber, 10)
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
 
 function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace('px', '');
-  let left = parseInt(leftNumbers, 10)
+  let leftNumber = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumber, 10)
   if (left > 0) {
     dodger.style.left = `${left + 1}px`
   }
